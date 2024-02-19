c0463ba65200a413011466763064cf8cd52ea6d3
Noah Breedy <breedyn@sunypoly.edu>
2024-02-19 12:18:42 -0500

    MainMenuState

---
+++ app/client/src/MainMenu.py
@@ -0,0 +1,20 @@
+class MainMenu:
+    def __init__(self,name):
+        self.name = name
+        self.state_machine = None
+        return
+    
+    def enter(self):
+        print(f"Entering: {self.name}")
+        return
+    
+    def leave(self):
+        print(f"Leaving: {self.name}")
+        return
+    
+    def render(self,window=None):
+        pass
+
+    def update(self):
+        pass
+    
