9c56147ee5c42bf15d669defa861c1b80f992665
simonliang4 <liangs2@sunypoly.edu>
2024-02-26 12:17:28 -0500

    Credit state updated

---
+++ app/client/src/CreditName.py
@@ -0,0 +1,56 @@
+import random
+import pygame
+
+class CreditName:
+    def __init__(self,x,y,width,height,text):
+        pygame.init()
+        self.rectX = x
+        self.rectY = y
+        self.height = height
+        self.width = width
+
+        font = pygame.font.SysFont('Georgia',30)
+        self.text= font.render(text, True, (255,255,255))
+        
+
+        randX = random.random()/3 +1
+        randY = random.random()/3 +1
+        randR = random.random() * 255
+        randG = random.random() * 255
+        randB = random.random() * 255
+        self.velX, self.velY = randX,randY
+        self.color = (randR,randG,randB)
+
+    def create_name(self):
+        name = pygame.Rect(self.rectX,self.rectY,self.width,self.height)
+        return name
+        
+
+    def bounce(self):
+        self.top = self.rectY +self.height
+        self.bottom = self.rectY
+        self.right = self.rectX +self.width
+        self.left = self.rectX
+
+        if (self.right >= 600) or (self.left <= 0):
+            self.velX = -self.velX
+        if (self.top >= 600) or (self.bottom <= 0):
+            self.velY = -self.velY
+
+    
+    def move(self):
+        self.rectX += self.velX
+        self.rectY += self.velY
+
+    def adjustment(self):
+        self.top = self.rectY +self.height
+        self.bottom = self.rectY
+        self.right = self.rectX +self.width
+        self.left = self.rectX
+
+
+        
+    
+
+
+    
--- app/client/src/CreditsState.py
+++ app/client/src/CreditsState.py
@@ -1,8 +1,19 @@
 import pygame
+import random
+from CreditName import CreditName
+
 class CreditsState:
+    
     def __init__(self,name):
+        pygame.init()
         self.name = name
         self.state_machine = None
+
+        
+        self.simonName = CreditName(10,10,190,50,'Simon Liang')
+        self.johnnyName = CreditName(400,150,190,50, 'John Pertell')
+        self.beckyName = CreditName(10,300,240,50, 'Becky Ostrander')
+        self.noahName = CreditName(400,450,190,50, 'Noah Breedy')
     
     def enter(self):
         print(f"Entering: {self.name}")
@@ -13,6 +24,88 @@ class CreditsState:
     def render(self,window=None):
         color = (0, 0, 0)
         window.fill(color)
+
+        self.simonRect = self.simonName.create_name()
+        self.johnnyRect = self.johnnyName.create_name()
+        self.beckyRect = self.beckyName.create_name()
+        self.noahRect = self.noahName.create_name()
+
+        self.nameCollide()
+
+        self.simonName.bounce()
+        pygame.draw.rect(window, (219,165,255), self.simonName.create_name())
+        self.simonName.move()
+
+        self.johnnyName.bounce()
+        pygame.draw.rect(window, self.johnnyName.color, self.johnnyName.create_name())
+        self.johnnyName.move()
+
+        self.beckyName.bounce()
+        pygame.draw.rect(window, self.beckyName.color, self.beckyName.create_name())
+        self.beckyName.move()
+
+        self.noahName.bounce()
+        pygame.draw.rect(window, self.noahName.color, self.noahName.create_name())
+        self.noahName.move()
+
+        
+        font = pygame.font.SysFont('Georgia',30) 
+        smallfont = pygame.font.SysFont('Georgia',25)
+
+        escText = font.render('Press ESC key to go back', True, (255,255,255))
+        creditText = font.render('Credits',True, (255,255,255))
+        devText = font.render('Developers', True, (255,255,255))
+
+        window.blit(escText,(20,560))
+        window.blit(creditText, (250,10))
+        window.blit(devText,(220,50))
+        window.blit(self.simonName.text,(self.simonName.rectX + 10, self.simonName.rectY+5))
+        window.blit(self.johnnyName.text,(self.johnnyName.rectX +10, self.johnnyName.rectY+5))
+        window.blit(self.beckyName.text,(self.beckyName.rectX+10, self.beckyName.rectY+5))
+        window.blit(self.noahName.text,(self.noahName.rectX+10, self.noahName.rectY+5))
+    
+    def nameCollide(self):
+
+        self.simonName.adjustment()
+        self.johnnyName.adjustment()
+        self.beckyName.adjustment()
+        self.noahName.adjustment()
+
+        if self.simonRect.colliderect(self.johnnyRect) or self.johnnyRect.colliderect(self.simonRect):
+            
+            self.simonName.velX, self.johnnyName.velX = -self.simonName.velX, -self.johnnyName.velX
+            self.simonName.velY ,self.johnnyName.velY = -self.simonName.velY, -self.johnnyName.velY
+            
+
+        if self.simonRect.colliderect(self.noahRect) or self.noahRect.colliderect(self.simonRect):
+            
+            self.simonName.velX, self.simonName.velY = -self.simonName.velX, -self.simonName.velY
+            self.noahName.velX, self.noahName.velY = -self.noahName.velX, -self.noahName.velY
+
+        if self.simonRect.colliderect(self.beckyRect) or self.beckyRect.colliderect(self.simonRect):
+            
+            self.simonName.velX, self.simonName.velY = -self.simonName.velX, -self.simonName.velY
+            self.beckyName.velX, self.beckyName.velY = -self.beckyName.velX, -self.beckyName.velY
+            
+
+        if self.johnnyRect.colliderect(self.beckyRect) or self.beckyRect.colliderect(self.johnnyRect):
+
+            self.johnnyName.velX, self.johnnyName.velY = -self.johnnyName.velX, -self.johnnyName.velY
+            self.beckyName.velX, self.beckyName.velY = -self.beckyName.velX, -self.beckyName.velY
+
+        if self.johnnyRect.colliderect(self.noahRect) or self.noahRect.colliderect(self.johnnyRect):
+
+            self.johnnyName.velX, self.johnnyName.velY = -self.johnnyName.velX, -self.johnnyName.velY
+            self.noahName.velX, self.noahName.velY = -self.noahName.velX, -self.noahName.velY
+
+        if self.beckyRect.colliderect(self.noahRect) or self.noahRect.colliderect(self.beckyRect):
+
+            self.beckyName.velX, self.beckyName.velY = -self.beckyName.velX, -self.beckyName.velY
+            self.noahName.velX, self.noahName.velY = -self.noahName.velX, -self.noahName.velY
+
+        
+            
+        
 
     def update(self):
         for event in pygame.event.get():
