d943b09dff8f4d34698bb470ca1303554d8f10b0
simonliang4 <liangs2@sunypoly.edu>
2024-03-02 06:13:13 -0500

    Basic NPC

---
+++ app/client/src/NPC.py
@@ -0,0 +1,31 @@
+import pygame
+
+
+
+class NPC:
+    def __init__(self,x,y,r):
+        self.x = x
+        self.y = y
+        self.tagged = False
+        self.position = [x,y]
+        self.color = (255,255,255)
+        self.radius = r
+        return
+    
+    def render(self,window):
+        pygame.draw.circle(window, self.color, (self.x,self.y),self.radius,0)
+        return
+    
+    def update(self,tagger_collision_check):
+        if (tagger_collision_check):
+            self.tagged = True
+
+        self.x = self.position[0]
+        self.y = self.position[1]
+        
+        return
+
+   
+        
+
+       
