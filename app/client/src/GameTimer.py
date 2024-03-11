a46b94c27a54b7a311834af5dddd56c34f39552e
Noah Breedy <breedyn@sunypoly.edu>
2024-03-11 13:24:25 -0400

    Fixed

--- app/client/src/GameTimer.py
+++ app/client/src/GameTimer.py
@@ -9,18 +9,23 @@ class GameTimer:
         self.color = color
         self.end_func = end_func
         self.font_size = 30
-    
+
     def render(self,window,debug,window_width):
+        t = 0
+        
         self.color = (255,255,255)
         if(debug):
             self.color = (0,0,0)
 
+        if(self.time >= 0):
+            t = self.time
+
         font = pygame.font.SysFont('Georgia',self.font_size)
-        text = font.render(f"Round-Timer: {self.time}", True, self.color) 
+        text = font.render(f"Round-Timer: {t}", True, self.color) 
         text_rect = text.get_rect()
         text_rect.center = ((window_width/2)-20, 30) 
         window.blit(text, text_rect)
-        
+
         return
         
     def update(self,num):
