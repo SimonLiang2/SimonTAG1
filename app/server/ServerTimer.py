48af51f344ffcb75691ec71d65f62cd791e46d82
Johnny <johnangus425@gmail.com>
2024-03-11 12:54:10 -0400

    timer sync

---
+++ app/server/ServerTimer.py
@@ -0,0 +1,20 @@
+import time as times
+
+class ServerTimer:
+    def __init__(self, time=15):
+        self.start_time = time
+        self.epoch_time = int(times.time())
+        self.time = time
+
+    def tick(self):
+        if self.time > 0:
+            self.time -= 1
+
+    def update(self):
+        if self.epoch_time < int(times.time()):
+            self.tick()
+            self.epoch_time = int(times.time())
+        return self.time
+
+    def reset(self):
+        self.time = self.start_time
